b08ce5a7dfe1a46af7cda17897e4a651db2f6189
Raphael Sofaer <rafi@sofaer.net>
2014-01-27 10:47:27 -0800

    Log return_code in a HydraWrapper fail

--- lib/hydra_wrapper.rb
+++ lib/hydra_wrapper.rb
@@ -55,7 +55,7 @@ class HydraWrapper
     @people.group_by { |person|
       @dispatcher_class.receive_url_for person
     }
-  end 
+  end
 
   # Prepares and inserts job into the hydra queue
   # @param url [String]
@@ -83,9 +83,9 @@ class HydraWrapper
           event: "http_multi_fail",
           sender_id: @user.id,
           url: response.effective_url,
-          response_code: response.code
+          response_code: response.code,
+          return_code: response.return_code
         }
-        message[:response_message] = response.return_message if response.code == 0
         Rails.logger.info message.to_a.map { |k,v| "#{k}=#{v}" }.join(' ')
         @failed_people += people_for_receive_url.map(&:id)
       end
